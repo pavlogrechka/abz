afdcac3356fe97747e100a4486f7e48ef9b5684e
pavlogrechka <pavlogrechka@gmail.com>
2019-12-13 21:42:05 +0200

    refact personal add bg menu

--- build/main.js
+++ build/main.js
@@ -1,6 +1,16 @@
 (function() {
-  const burger = document.querySelector(".burger");
-  burger.addEventListener("click", () => {
-    document.body.classList.toggle("menu-open");
-  });
+  const burger = document.querySelector(".js-burger");
+  const closeBg = document.querySelector(".js-bg");
+  const burgerOpenClassName = "menu-open";
+  // const fnToggleBurger = () => {
+  //   document.body.classList.toggle("menu-open");
+  // };
+  const fnCloseBurger = () => {
+    document.body.classList.remove(burgerOpenClassName);
+  };
+  const fnOpenBurger = () => {
+    document.body.classList.add(burgerOpenClassName);
+  };
+  burger.addEventListener("click", fnOpenBurger);
+  closeBg.addEventListener("click", fnCloseBurger);
 })();
